1b8765ed84cf3c101830efa83993be3eb59b5a56
Loris Lucido <loris.lucido@bull.com>
2026-07-09 14:49:01 +0200

    modquadrant, modcrosssection, modlsmcrosssection, modcape: uncomment omp gpu update

--- src/program.f90
+++ src/program.f90
@@ -376,7 +376,6 @@ program DALES
           call genstat  !Genstat must preceed all other statistics that could write in the same netCDF file (unless stated otherwise
           call write_profiles
           call radstat
-          host_is_updated=.false.; call update_host
           call lsmstat
           !call depstat
           call sampling
@@ -389,6 +388,7 @@ program DALES
           call docape
           !call projection
           call cloudfield
+          host_is_updated=.false.; call update_host
           call fielddump
           call radfield
           !call particles
