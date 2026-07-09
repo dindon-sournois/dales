052031c7cb3244d5187811a95dc7d70bbd673577
Loris Lucido <loris.lucido@bull.com>
2026-07-09 15:22:45 +0200

    tstep_update omp gpu

--- src/program.f90
+++ src/program.f90
@@ -250,9 +250,11 @@ program DALES
   do while (timeleft > 0)
     do simid = 1, Nsim
 
+      call update_gpu
       if (simid == refid) call tstep_update
 
       do rk3step = 1, 3
+        call update_gpu
         call timer_tic('program/timestep', istep)
     
     
@@ -279,7 +281,6 @@ program DALES
     !-----------------------------------------------------
     !   3.2   RADIATION
     !-----------------------------------------------------
-        call update_gpu
         call radiation !radiation scheme
         call samptend(tend_rad)
     
@@ -403,9 +404,9 @@ program DALES
         end if
 
         call reset_tendencies
-        host_is_updated=.false.; call update_host
 
-#if defined(_OPENACC)
+        host_is_updated=.false.; call update_host
+#if defined(DALES_GPU)
         host_is_updated = .false.
 #endif
         call timer_toc('program/timestep')
